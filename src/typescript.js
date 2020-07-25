b6ceb867ebb652af14d62118df3911200d318195
aminya <aminyahyaabadi74@gmail.com>
2020-07-25 09:59:51 -0500

    fix: disable checkAllFilesOnSave

--- src/typescript.js
+++ src/typescript.js
@@ -2,6 +2,8 @@
 export function setupTypeScript() {
   // use atom-typescript for javascript
   atom.config.set("atom-typescript.allowJS", true)
+  // disable check files which slows down Atom
+  atom.config.set("atom-typescript.checkAllFilesOnSave", false)
   // activate atom-typescript
   atom.commands.dispatch(atom.workspace.getElement(), "typescript:activate")
 }
