2acb918f0d4c83a8d09f4a814fe66c54c6da6c3d
aminya <aminyahyaabadi74@gmail.com>
2020-10-12 07:16:51 -0500

    feat: support flow syntax

--- src/typescript.js
+++ src/typescript.js
@@ -6,4 +6,57 @@ export function setupTypeScript() {
   atom.config.set("atom-typescript.checkAllFilesOnSave", false)
   // activate atom-typescript
   atom.commands.dispatch(atom.workspace.getElement(), "typescript:activate")
+  // support flow in JavaScript files
+  atom.config.set("atom-typescript.ignoredDiagnosticCodes", ["8002", "8003", "8004", "8006", "8008", "8010"])
 }
+
+/*
+"'import ... =' can only be used in TypeScript files.": {
+    "category": "Error",
+    "code": 8002
+},
+"'export =' can only be used in TypeScript files.": {
+    "category": "Error",
+    "code": 8003
+},
+"Type parameter declarations can only be used in TypeScript files.": {
+    "category": "Error",
+    "code": 8004
+},
+"'implements' clauses can only be used in TypeScript files.": {
+    "category": "Error",
+    "code": 8005
+},
+"'{0}' declarations can only be used in TypeScript files.": {
+    "category": "Error",
+    "code": 8006
+},
+"Type aliases can only be used in TypeScript files.": {
+    "category": "Error",
+    "code": 8008
+},
+"The '{0}' modifier can only be used in TypeScript files.": {
+    "category": "Error",
+    "code": 8009
+},
+"Type annotations can only be used in TypeScript files.": {
+    "category": "Error",
+    "code": 8010
+},
+"Type arguments can only be used in TypeScript files.": {
+    "category": "Error",
+    "code": 8011
+},
+"Parameter modifiers can only be used in TypeScript files.": {
+    "category": "Error",
+    "code": 8012
+},
+"Non-null assertions can only be used in TypeScript files.": {
+    "category": "Error",
+    "code": 8013
+},
+"Type assertion expressions can only be used in TypeScript files.": {
+    "category": "Error",
+    "code": 8016
+},
+*/
